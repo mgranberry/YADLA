ff748b2d099e6c3dd7eb55a922b5d4de7062ba26
Matthias Granberry <matthias.granberry@gmail.com>
2015-07-12 09:35:57 -0500

    adaptive entry count on NightScout sync (hopefully fewer missed records)

--- app/src/main/java/com/kludgenics/cgmlogger/app/service/NightscoutEntryTask.kt
+++ app/src/main/java/com/kludgenics/cgmlogger/app/service/NightscoutEntryTask.kt
@@ -12,6 +12,8 @@ import io.realm.Realm
 import org.jetbrains.anko.AnkoLogger
 import org.joda.time.DateTime
 import kotlin.properties.Delegates
+import com.kludgenics.cgmlogger.extension.*
+import org.joda.time.Period
 
 /**
  * Created by matthiasgranberry on 6/4/15.
@@ -19,11 +21,21 @@ import kotlin.properties.Delegates
 
 class NightscoutEntryTask(override val ctx: Context,
                           override val nightscoutEndpoint: NightscoutApiEndpoint,
-                          val count: Int = 10): NightscoutTask, AnkoLogger {
+                          val count: Int = 0): NightscoutTask, AnkoLogger {
 
     override val init: NightscoutApiEndpoint.() -> List<SgvEntry>
         get() = fun (): List<SgvEntry> {
-            return getSgvEntries(count)
+            val requestCount = if (count == 0) {
+                val realm = Realm.getDefaultInstance()
+                val latestTime = DateTime(realm.use {
+                    realm.where<BloodGlucoseRecord> {
+                        this
+                    }.findAllSorted("date", false).first().date
+                })
+                val difference = Period(latestTime, DateTime())
+                1 + difference.getMinutes() / 5
+            } else count
+            return getSgvEntries(requestCount)
         }
 
     override val copy: Realm.(Any) -> Unit

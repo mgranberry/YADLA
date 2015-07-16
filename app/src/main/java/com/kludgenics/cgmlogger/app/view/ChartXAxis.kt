dc68e509815a9290313b99d55a3b38cb11c6602d
Matthias Granberry <matthias.granberry@gmail.com>
2015-07-16 14:23:43 -0500

    add text to bounds, don't label last tick

--- app/src/main/java/com/kludgenics/cgmlogger/app/view/ChartXAxis.kt
+++ app/src/main/java/com/kludgenics/cgmlogger/app/view/ChartXAxis.kt
@@ -6,7 +6,7 @@ import android.graphics.*
  * Created by matthiasgranberry on 7/16/15.
  */
 
-interface AxisChartView<X: Comparable<X>,Y: Comparable<Y>> {
+interface ChartXAxis<X: Comparable<X>> {
 
     var xProgression: Progression<X>
     var xLabelPeriod: Int
@@ -22,10 +22,12 @@ interface AxisChartView<X: Comparable<X>,Y: Comparable<Y>> {
     fun getXValue(xValue: X): Float
     fun getXLabel(xValue: X): String
 
-    fun getXAxisHeight(): Float {
-        val bounds = RectF()
+    fun getXAxisBounds(bounds: RectF) {
+        val label = getXLabel(xProgression.start)
+        val textBounds = Rect()
         xAxisPath.computeBounds(bounds, false)
-        return bounds.bottom - bounds.top
+        xAxisPaint.getTextBounds(label, 0, label.length(), textBounds)
+        bounds.bottom = textBounds.bottom.toFloat() + bounds.bottom
     }
 
     fun updateXaxisPath() {
@@ -44,11 +46,11 @@ interface AxisChartView<X: Comparable<X>,Y: Comparable<Y>> {
         xAxisPath.transform(scaleMatrix)
     }
 
-    fun drawAxes(canvas: Canvas) {
+    fun drawXAxis(canvas: Canvas) {
         if (showXAxis) {
             canvas.drawPath(xAxisPath, xAxisPaint)
             xProgression.forEachIndexed { idx, x ->
-                if (idx % xLabelPeriod == 0)
+                if (idx % xLabelPeriod == 0 && x != xProgression.end)
                     canvas.drawText(getXLabel(x), getXValue(x), xAxisOffset+xAxisTickHeight, xAxisPaint)}
         }
     }

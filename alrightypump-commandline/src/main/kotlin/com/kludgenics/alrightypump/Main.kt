18bc4b21a714c100dbfb1c201fdd36a67130ec63
Matthias Granberry <matthias.granberry@gmail.com>
2015-12-08 23:28:52 -0600

    threaded uploader

--- alrightypump-commandline/src/main/kotlin/com/kludgenics/alrightypump/Main.kt
+++ alrightypump-commandline/src/main/kotlin/com/kludgenics/alrightypump/Main.kt
@@ -4,40 +4,54 @@ import com.fazecast.jSerialComm.SerialPort
 import com.kludgenics.alrightypump.cloud.nightscout.Nightscout
 import com.kludgenics.alrightypump.device.dexcom.g4.DexcomG4
 import com.kludgenics.alrightypump.device.tandem.TandemPump
-import com.kludgenics.alrightypump.therapy.TreeMapTherapyTimeline
+import com.kludgenics.alrightypump.therapy.ConcurrentSkipListTherapyTimeline
+import com.kludgenics.alrightypump.therapy.NavigableMapTherapyTimeline
 import com.squareup.okhttp.HttpUrl
 import org.joda.time.DateTime
+import org.joda.time.Duration
+import org.joda.time.Instant
 import org.joda.time.Period
+import kotlin.concurrent.thread
 
 
 fun main(args: Array<String>) {
+    val start = Instant()
     val ports = SerialPort.getCommPorts()
-    val timeline = TreeMapTherapyTimeline()
+    val timeline = ConcurrentSkipListTherapyTimeline()
     val startTime = DateTime.now() - Period.months(3)
+    val threads = arrayListOf<Thread>()
     ports.forEach {
         println("Looking for device on ${it.descriptivePortName}")
         when (it.descriptivePortName) {
             "Tandem Virtual COM Port" -> {
-                println("Found Tandem pump. Fetching data.")
-                it.baudRate = 115200 * 12
-                val connection = SerialConnection(it)
-                connection.use {
-                    val tslim = TandemPump(connection.source(), connection.sink())
-                    timeline.merge({ it.time >= startTime }, tslim.basalRecords, tslim.bolusRecords,
-                            tslim.smbgRecords, tslim.consumableRecords)
-                }
+                threads.add(thread {
+                    println("Found Tandem pump. Fetching data.")
+                    it.baudRate = 115200 * 12
+                    val connection = SerialConnection(it)
+                    connection.use {
+                        val tslim = TandemPump(connection.source(), connection.sink())
+                        timeline.merge({ it.time >= startTime }, tslim.basalRecords, tslim.bolusRecords,
+                                tslim.smbgRecords, tslim.consumableRecords)
+                    }
+                })
             }
             "DexCom Gen4 USB Serial" -> {
-                println("Found DexCom G4.  Fetching data.")
-                val connection = SerialConnection(it)
-                connection.use {
-                    val g4 = DexcomG4(connection.source(), connection.sink())
-                    timeline.merge({ it.time >= startTime }, g4.cgmRecords, g4.smbgRecords, g4.calibrationRecords,
-                            g4.eventRecords, g4.consumableRecords)
-                }
+                threads.add(thread {
+                    println("Found DexCom G4.  Fetching data.")
+                    val connection = SerialConnection(it)
+                    connection.use {
+                        val g4 = DexcomG4(connection.source(), connection.sink())
+                        timeline.merge({ it.time >= startTime }, g4.cgmRecords, g4.smbgRecords, g4.calibrationRecords,
+                                g4.eventRecords, g4.consumableRecords)
+                    }
+                })
             }
         }
     }
+
+    threads.forEach { println("join"); it.join(); println("joined") }
+    val mid = Instant()
+    println("Time to read from devices: ${Duration(start, mid)}")
 
     val nightscout_url = System.getenv("NIGHTSCOUT_HOST") ?: args.getOrNull(0)
     if (nightscout_url == null) {
@@ -52,4 +66,6 @@ fun main(args: Array<String>) {
         }
         nightscout.okHttpClient.dispatcher.executorService.shutdown()
     }
+    val end = Instant()
+    println("Time to upload: ${Duration(mid, end)}")
 }
--- alrightypump/src/main/kotlin/com/kludgenics/alrightypump/therapy/TherapyTimeline.kt
+++ alrightypump/src/main/kotlin/com/kludgenics/alrightypump/therapy/TherapyTimeline.kt
@@ -1,6 +1,9 @@
 package com.kludgenics.alrightypump.therapy
 
 import org.joda.time.Instant
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentSkipListMap
 
 /**
  * Created by matthias on 11/22/15.
@@ -15,4 +18,37 @@ interface TherapyTimeline {
     fun events(start: Instant, end: Instant = Instant.now()): Collection<Record>
     fun merge(vararg additionalEvents: Sequence<Record>)
     fun merge(predicate: (Record) -> Boolean, vararg additionalEvents: Sequence<Record>)
+}
+
+
+abstract class AbstractMapTherapyTimeline: TherapyTimeline {
+    protected abstract val _events: NavigableMap<Instant, MutableSet<Record>>
+    protected abstract val _eventSet: () -> MutableSet<Record>
+    override val events: Sequence<Record> get() = _events.values.flatMap { it }.asSequence()
+    override val bolusEvents: Sequence<BolusRecord> get() = events.filterIsInstance<BolusRecord>()
+    override val glucoseEvents: Sequence<GlucoseRecord> get() = events.filterIsInstance<GlucoseRecord>()
+    override val basalEvents: Sequence<BasalRecord> get() = events.filterIsInstance<BasalRecord>()
+    override fun events(start: Instant, end: Instant): List<Record> =
+            _events.tailMap(start).headMap(end).values.flatMap { it }
+
+    override fun merge(vararg additionalEvents: Sequence<Record>) {
+        merge({true}, *additionalEvents)
+    }
+
+    override fun merge(predicate: (Record) -> Boolean, vararg additionalEvents: Sequence<Record>) {
+        val e = additionalEvents.map { it }
+        val events = additionalEvents.flatMap { it.takeWhile(predicate).toList() }
+        for (event in events)
+            _events.getOrPut(event.time, _eventSet ).add(event)
+    }
+}
+
+open class NavigableMapTherapyTimeline : AbstractMapTherapyTimeline() {
+    protected override val _events: NavigableMap<Instant, MutableSet<Record>> = TreeMap()
+    protected override val _eventSet: () -> MutableSet<Record> = { HashSet(1) }
+}
+
+class ConcurrentSkipListTherapyTimeline : AbstractMapTherapyTimeline() {
+    protected override val _events: NavigableMap<Instant, MutableSet<Record>> = ConcurrentSkipListMap()
+    protected override val _eventSet = { Collections.newSetFromMap(ConcurrentHashMap<Record, Boolean>())}
 }

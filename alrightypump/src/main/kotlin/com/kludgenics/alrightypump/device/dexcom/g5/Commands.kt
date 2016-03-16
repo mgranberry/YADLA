96d3d10fd2e2e74698f7dc998010e01d57549174
Matthias Granberry <matthias.granberry@gmail.com>
2016-03-16 13:02:15 -0500

    add raw G5 command/responses

--- alrightypump/src/main/kotlin/com/kludgenics/alrightypump/device/dexcom/g5/Commands.kt
+++ alrightypump/src/main/kotlin/com/kludgenics/alrightypump/device/dexcom/g5/Commands.kt
@@ -5,19 +5,24 @@ import okio.*
 /**
  * Created by matthias on 11/24/15.
  */
+object Opcodes {
+    const final val AUTH_REQUEST_COMMAND = 0x01
+    const final val AUTH_CHALLENGE_RESPONSE = 0x03
+    const final val AUTH_CHALLENGE_COMMAND = 0x04
+    const final val AUTH_STATUS_RESPONSE = 0x05
+    const final val KEEPALIVE_COMMAND = 0x06
+    const final val BOND_COMMAND = 0x07
+    const final val DISCONNECT_COMMAND = 0x09
+    const final val TRANSMITTER_TIME_COMMAND = 0x24
+    const final val TRANSMITTER_TIME_RESPONSE = 0x25
+    const final val GLUCOSE_COMMAND = 0x30
+    const final val GLUCOSE_RESPONSE = 0x31
+    const final val SENSOR_COMMAND = 0x2e
+    const final val SENSOR_RESPONSE = 0x2f
+}
 
 interface DexcomG5Command : Source {
     val payload: ByteString
-
-    companion object {
-        const final val OPCODE_AUTH_REQUEST_COMMAND = 0x01
-        const final val OPCODE_AUTH_CHALLENGE_COMMAND = 0x04
-        const final val OPCODE_KEEPALIVE_COMMAND = 0x06
-        const final val OPCODE_BOND_COMMAND = 0x07
-        const final val OPCODE_DISCONNECT_COMMAND = 0x09
-        const final val OPCODE_TRANSMITTER_TIME_COMMAND = 0x24
-        const final val OPCODE_GLUCOSE_COMMAND = 0x30
-    }
 
     override fun timeout(): Timeout {
         return Timeout.NONE
@@ -34,30 +39,30 @@ interface DexcomG5Command : Source {
 
 interface DexcomG5Response {
     companion object {
-        const final val OPCODE_AUTH_CHALLENGE_RESPONSE = 0x03
-        const final val OPCODE_AUTH_STATUS_RESPONSE = 0x05
-        const final val OPCODE_GLUCOSE_RESPONSE = 0x31
-        const final val OPCODE_TRANSMITTER_TIME_RESPONSE = 0x25
 
         fun parse(source: BufferedSource): DexcomG5Response {
             source.require(1)
             val opcode = source.readByte().toInt() and 0xFF
             val result = when (opcode) {
-                OPCODE_AUTH_CHALLENGE_RESPONSE -> {
+                Opcodes.AUTH_CHALLENGE_RESPONSE -> {
                     source.require(16)
                     AuthChallengeResponse(opcode, source)
                 }
-                OPCODE_AUTH_STATUS_RESPONSE -> {
+                Opcodes.AUTH_STATUS_RESPONSE -> {
                     source.require(2)
                     AuthStatusResponse(opcode, source)
                 }
-                OPCODE_GLUCOSE_RESPONSE -> {
+                Opcodes.GLUCOSE_RESPONSE -> {
                     source.require(13)
                     GlucoseResponse(opcode, source)
                 }
-                OPCODE_TRANSMITTER_TIME_RESPONSE -> {
+                Opcodes.TRANSMITTER_TIME_RESPONSE -> {
                     source.require(9)
                     TransmitterTimeResponse(opcode, source)
+                }
+                Opcodes.SENSOR_RESPONSE -> {
+                    source.require(16)
+                    SensorResponse(opcode, source)
                 }
                 else -> UnknownResponse(opcode, source)
             }
@@ -72,7 +77,7 @@ interface DexcomG5Response {
 
 data class AuthChallengeCommand(public val challengeHash: ByteString) : DexcomG5Command {
     override val payload: ByteString by lazy {
-        Buffer().writeByte(DexcomG5Command.OPCODE_AUTH_CHALLENGE_COMMAND)
+        Buffer().writeByte(Opcodes.AUTH_CHALLENGE_COMMAND)
                 .write(challengeHash)
                 .snapshot()
     }
@@ -90,7 +95,7 @@ data class AuthChallengeResponse(public val opcode: Int,
 data class AuthRequestCommand(public val singleUseToken: ByteString,
                               public val token: ByteString) : DexcomG5Command {
     override val payload: ByteString by lazy {
-        Buffer().writeByte(DexcomG5Command.OPCODE_AUTH_REQUEST_COMMAND)
+        Buffer().writeByte(Opcodes.AUTH_REQUEST_COMMAND)
                 .write(token)
                 .writeByte(0x02)
                 .snapshot()
@@ -107,13 +112,13 @@ data class AuthStatusResponse(public val opcode: Int,
 
 class BondRequestCommand : DexcomG5Command {
     override val payload: ByteString by lazy {
-        Buffer().writeByte(DexcomG5Command.OPCODE_BOND_COMMAND).snapshot()
+        Buffer().writeByte(Opcodes.BOND_COMMAND).snapshot()
     }
 }
 
 class DisconnectCommand : DexcomG5Command {
     override val payload: ByteString by lazy {
-        Buffer().writeByte(DexcomG5Command.OPCODE_DISCONNECT_COMMAND).snapshot()
+        Buffer().writeByte(Opcodes.DISCONNECT_COMMAND).snapshot()
     }
 }
 
@@ -135,7 +140,7 @@ class GlucoseResponse(public val opcode: Int,
 
 class GlucoseCommand : DexcomG5Command {
     override val payload: ByteString by lazy {
-        Buffer().writeByte(DexcomG5Command.OPCODE_GLUCOSE_COMMAND)
+        Buffer().writeByte(Opcodes.GLUCOSE_COMMAND)
                 .writeShortLe(0x3653) // CRC
                 .snapshot()
     }
@@ -143,7 +148,7 @@ class GlucoseCommand : DexcomG5Command {
 
 data class KeepaliveCommand(public val time: Int) : DexcomG5Command {
     override val payload: ByteString by lazy {
-        Buffer().writeByte(DexcomG5Command.OPCODE_KEEPALIVE_COMMAND)
+        Buffer().writeByte(Opcodes.KEEPALIVE_COMMAND)
                 .writeByte(time)
                 .snapshot()
     }
@@ -151,7 +156,7 @@ data class KeepaliveCommand(public val time: Int) : DexcomG5Command {
 
 class TransmitterTimeCommand : DexcomG5Command {
     override val payload: ByteString by lazy {
-        Buffer().writeByte(DexcomG5Command.OPCODE_TRANSMITTER_TIME_COMMAND)
+        Buffer().writeByte(Opcodes.TRANSMITTER_TIME_COMMAND)
                 .writeShortLe(0x64e6) // CRC
                 .snapshot()
     }
@@ -169,5 +174,36 @@ class TransmitterTimeResponse(public val opcode: Int,
 
 class UnknownResponse(public val opcode: Int,
                       public val contents: ByteString) : DexcomG5Response {
-    constructor(opcode: Int, source: BufferedSource) : this(opcode, source.readByteString())
+    constructor(opcode: Int, source: BufferedSource) : this(opcode,
+            contents=source.readByteString())
+}
+
+class SensorCommand : DexcomG5Command {
+    override val payload: ByteString by lazy {
+        Buffer().writeByte(Opcodes.SENSOR_COMMAND)
+                .writeShortLe(CRC.calculateCRC(Opcodes.SENSOR_COMMAND)) // CRC
+                .snapshot()
+    }
+}
+
+class SensorResponse(public val opcode: Int,
+                     public val status: Int,
+                     public val timestamp: Int,
+                     public val unfiltered: Int,
+                     public val filtered: Int) : DexcomG5Response {
+    constructor(opcode: Int, source: BufferedSource) : this(opcode=opcode,
+            status=source.readByte().toInt() and 0xFF,
+            timestamp=source.readIntLe(),
+            unfiltered=source.readIntLe(),
+            filtered=source.readIntLe())
+}
+
+object CRC {
+    fun calculateCRC(byte: Int): Int
+    {
+        var crc = 0x00;
+        var x = crc shr 8 xor (byte and 0xFF)
+        x = x xor (x shr 4)
+        return ((crc shl 8) xor (x shl 12) xor (x shl 5) xor (x)) and 0xFFFF
+    }
 }
